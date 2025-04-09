974fbccca8f78de292727e747161228d25b0bbd1
notastay <notastay@gmail.com>
2025-04-09 10:29:44 +0200

    10:29

--- src/main/java/notastay/ColorPickerController.java
+++ src/main/java/notastay/ColorPickerController.java
@@ -1,6 +1,7 @@
 package notastay;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
@@ -30,6 +31,9 @@ public class ColorPickerController {
         redSlider.valueProperty().addListener((observable, oldValue, newValue)-> updateFromSlider(redSlider, redTField));
         greenSlider.valueProperty().addListener((observable, oldValue, newValue)-> updateFromSlider(greenSlider, greenTField));
         blueSlider.valueProperty().addListener((observable, oldValue, newValue)-> updateFromSlider(blueSlider, blueTField));
+        redTField.textProperty().addListener((observable, oldValue, newValue)-> updateFromTField(redTField, redSlider));
+        greenTField.textProperty().addListener((observable, oldValue, newValue)-> updateFromTField(greenTField, greenSlider));
+        blueTField.textProperty().addListener((observable, oldValue, newValue)-> updateFromTField(blueTField, blueSlider));
     }
 
     private void updateFromSlider(Slider slider, TextField textField) {
@@ -46,6 +50,37 @@ public class ColorPickerController {
         String hexColor= String.format("#%02X%02X%02X", red, green, blue);
         colorPanel.setStyle("-fx-background-color: rgb("+red+","+green+","+blue+");");
         hexLabel.setText(hexColor);
+    }
+
+    private void updateFromTField(TextField tField, Slider slider){
+        if(isValid(tField.getText())){
+            int value= Integer.parseInt(tField.getText());
+            if(value<256 && value>=0){
+                slider.setValue(value);
+                updateColor();
+            }else{
+                showAlert();
+        }
+        }else {
+            showAlert();
+        }
+    }
+
+    private boolean isValid(String text){
+        try {
+            int value = Integer.parseInt(text);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    private void showAlert() {
+        Alert alerta= new Alert(Alert.AlertType.ERROR);
+        alerta.setTitle("ERROR");
+        alerta.setHeaderText("Color Invalido");
+        alerta.setContentText("El color debe ser un numero entre 0 y 255");
+        alerta.showAndWait();
     }
 
 }
