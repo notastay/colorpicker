cfca918b0657e43f92c4366eae0a7fad96723bb1
notastay <151544079+notastay@users.noreply.github.com>
2025-04-09 12:08:54 +0200

    12:08

--- src/main/java/notastay/App.java
+++ src/main/java/notastay/App.java
@@ -19,6 +19,7 @@ public class App extends Application {
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("colorPicker"), 600, 400);
         stage.setResizable(false);
+        stage.setTitle("Nadia Henrich Romero");
         stage.setScene(scene);
         stage.show();
     }
