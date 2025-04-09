7da56c435d8b80b695ed7c54294d669bc703b94e
notastay <notastay@gmail.com>
2025-04-09 10:12:55 +0200

    10:12

--- src/main/java/notastay/ColorPickerController.java
+++ src/main/java/notastay/ColorPickerController.java
@@ -7,7 +7,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 
 public class ColorPickerController {
-    
+
     @FXML
     Pane colorPanel;
     @FXML
@@ -24,5 +24,26 @@ public class ColorPickerController {
     TextField greenTField;
     @FXML
     TextField blueTField;
+
+    @FXML
+    public void initialize() {
+        redSlider.valueProperty().addListener((observable, oldValue, newValue)-> updateFromSlider(redSlider, redTField));
+    }
+
+    private void updateFromSlider(Slider slider, TextField textField) {
+        int value = (int) slider.getValue();
+        textField.setText(String.valueOf(value));
+        updateColor();
+    }
+
+    private void updateColor() {
+        int red=(int) redSlider.getValue();
+        int green=(int) greenSlider.getValue();
+        int blue=(int) blueSlider.getValue();
+
+        String hexColor= String.format("#%02X%02X%02X", red, green, blue);
+        colorPanel.setStyle("-fx-background-color: rgb("+red+","+green+","+blue+");");
+        hexLabel.setText(hexColor);
+    }
 
 }
